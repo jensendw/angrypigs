d688e06c12372566e45de3e5538419721f5592d9
Daniel Jensen <jensen@origamilogic.com>
2016-05-13 12:19:31 -0700

    first commit

---
+++ api.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+
+app = Flask(__name__)
+api = Api(app)
+APP_DEBUG = True
+
+
+class Health(Resource):
+    def get(self):
+        return {'status': 'OK'}
+
+
+class Default(Resource):
+    def get(self):
+        return "Welcome to AngryPigs"
+
+
+api.add_resource(Health, '/health')
+api.add_resource(Default, '/')
+
+if __name__ == '__main__':
+    app.run(debug=APP_DEBUG, host='0.0.0.0')
